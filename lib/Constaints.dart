f8faa9daff66038ec528f73eaaa9e7d3dd3aa1f0
Adnan Haider <adnanhaider3783@gmail.com>
2021-07-19 17:55:39 +0500

    up

---
+++ lib/Constaints.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+var kTestMargin =EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0);
+var kTestPadding =EdgeInsets.symmetric(horizontal: 12.0);
+
+
+
+var kBoxDecorate = BoxDecoration(
+  borderRadius: BorderRadius.circular(8.0),
+  border: Border.all(color: Color(0xffFC9425)),
+  color: Color(0xffFC9425),
+);
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
-import 'package:topi/starter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
+import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
+import 'package:topi/Constaints.dart';
+import 'package:topi/starter.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
@@ -33,12 +35,12 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
 
   bool isClick=false;
   bool isVisible=false;
   var texts='not Click', iconsValue=Icons.keyboard_arrow_down;
-
+  var typeId=1;
   onClick(){
     setState(() {
       if(isClick){
@@ -58,15 +60,36 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
 
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    animationController = AnimationController(vsync: this, duration: Duration(seconds: 25),
+    );
+    animationController.addListener(() {
+      setState(() {
+
+      });
+    });
+
+    //animationController.repeat();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final percentage = animationController.value * 100;
+
     return SafeArea(
       child: Scaffold(
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: FloatingActionButton(
           backgroundColor: Color(0xffFC9425),
           onPressed: () {  },
-          child: Text('T', style: TextStyle(
+          child: Text('T',
+            style: TextStyle(
             fontFamily: 'FredokaOne',
             fontSize: 40.0
           ),),
@@ -112,23 +135,29 @@ class _SplashScreenState extends State<SplashScreen> {
                 ),
               ),
               Container(
-                height: 40,
-                  decoration: BoxDecoration(
-                    color: Color(0xffFC9425),
-                  border: Border.all(color: Color(0xffFC9425)),
-                  borderRadius: BorderRadius.circular(8),
+                decoration:kBoxDecorate,
+                margin:kTestMargin,
+                child: DropdownButtonHideUnderline(
+                  child: Padding(
+                    padding: kTestPadding,
+                    child: DropdownButton<String>(
+                      value: '$typeId',
+                      isExpanded: true,
+                      icon: Icon(
+                        iconsValue,
+                        color: Colors.white,
+                      ),
+                      items: getDropDownTypeItem(),
+                      dropdownColor: Color(0xffFC9425),
+                      onChanged: (value) {
+                        setState(() {
+                          typeId = int.parse(value!);
+                          isClick=true;
+                        });
+                      },
+                    ),
+                  ),
                 ),
-                margin: EdgeInsets.all(12.0),
-                child: ListTile(
-                  visualDensity: VisualDensity(vertical: -4),
-                  leading: Text(texts,style: TextStyle(color: Colors.white),),
-                  trailing:Icon(iconsValue,color: Colors.white,) ,
-                  onTap:() {
-                    setState(() {
-                      onClick();
-                    });
-                  } ,
-                )
               ),
               Visibility(
                 visible: isClick,
@@ -141,13 +170,12 @@ class _SplashScreenState extends State<SplashScreen> {
                       return Column(
                         children: <Widget>[
                           Container(
-                              height: 40,
                               decoration: BoxDecoration(
                                 color: Color(0xffFC9425),
                                 border: Border.all(color: Color(0xffFC9425)),
                                 borderRadius: BorderRadius.circular(8),
                               ),
-                              margin: EdgeInsets.only(top: 4.0,right: 12.0,left: 12.0),
+                              margin: EdgeInsets.only(top: 4.0,right: 16.0,left: 16.0),
                               child: ListTile(
                                 visualDensity: VisualDensity(vertical: -4),
                                 leading: Text('List of songs category',style: TextStyle(color: Colors.white),),
@@ -159,44 +187,28 @@ class _SplashScreenState extends State<SplashScreen> {
                                 } ,
                               )
                           ),
-                            Divider(height: 8,indent: 14,endIndent: 14,)
-                        ],
-                      );
-                    }),
-              ),
-              Visibility(
-                visible: isVisible,
-                child: ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: 15,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Column(
-                        children: <Widget>[
-                          Container(
-                              height: 40,
-                              decoration: BoxDecoration(
-                                color: Color(0xffFC9425),
-                                border: Border.all(color: Color(0xffFC9425)),
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              margin: EdgeInsets.only(top: 4.0,right: 12.0,left: 12.0),
-                              child: ListTile(
-                                visualDensity: VisualDensity(vertical: -4),
-                                leading: Text('List of songs',style: TextStyle(color: Colors.white),),
-                                onTap:() {
-                                  setState(() {
-                                    isVisible=false;
-                                    texts='not clicked';
-                                  });
-                                } ,
-                              )
-                          ),
                           Divider(height: 8,indent: 14,endIndent: 14,)
                         ],
                       );
                     }),
+              ),
+              Container(
+                width: double.infinity,
+                height: 50,
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: LiquidLinearProgressIndicator(
+                  borderRadius: 12.0,
+                  value: animationController.value,
+                  valueColor: AlwaysStoppedAnimation(Color(0xffFC9425)),
+                  center: Text('${percentage.toStringAsFixed(0)}%',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),),
+                  direction: Axis.horizontal,
+                  backgroundColor: Colors.grey.shade300,
+                ),
               ),
             ],
           ),
@@ -205,4 +217,20 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 
+  List<DropdownMenuItem<String>> getDropDownTypeItem() {
+    List<DropdownMenuItem<String>> dropDown = [];
+    for (int i = 0; i <12; i++) {
+      var id = i;
+      var text ='class_name';
+      var newWidget = DropdownMenuItem(
+        child: Text(
+          '$text',
+          style: TextStyle(fontSize: 18.0, color: Colors.white),
+        ),
+        value: '$id',
+      );
+      dropDown.add(newWidget);
+    }
+    return dropDown;
+  }
 }
